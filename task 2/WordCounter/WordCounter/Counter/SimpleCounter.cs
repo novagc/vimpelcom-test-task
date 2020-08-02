cc2be398f39a9d8b85c7ec43a174ae0ce869ad37
novagc <novagc@yandex.ru>
2020-08-02 22:42:12 +0900

    add task 2 counter

---
+++ task 2/WordCounter/WordCounter/Counter/SimpleCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordCounter.File;
+using WordCounter.Interfaces;
+
+namespace WordCounter.Counter
+{
+    public class SimpleCounter : ICounter
+    {
+        public IReader Reader { get; set; }
+
+        public SimpleCounter(IReader reader)
+        {
+            Reader = reader;
+        }
+
+        public Dictionary<string, int> Count()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            foreach (string word in Reader.GetWords())
+            {
+                if (!result.ContainsKey(word))
+                {
+                    result.Add(word, 1);
+                }
+                else
+                {
+                    result[word]++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
