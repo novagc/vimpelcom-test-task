55b8ec4feac9eacb52e8939cdba961ff06ebd603
novagc <novagc@yandex.ru>
2020-08-02 22:41:49 +0900

    add task 2 io classes

---
+++ task 2/WordCounter/WordCounter/File/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WordCounter.Interfaces;
+
+namespace WordCounter.File
+{
+    public class FileReader : IReader
+    {
+        public ISource Source { get; set; }
+
+        public FileReader(ISource source)
+        {
+            Source = source;
+        }
+
+        public IEnumerable<string> GetWords() => (IEnumerable<string>)new Regex(@"\w{10,}").Matches(Source.Read());
+    }
+}
---
+++ task 2/WordCounter/WordCounter/File/FileSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using WordCounter.Interfaces;
+
+namespace WordCounter.File
+{
+    public class FileSource : ISource
+    {
+        private readonly string path;
+
+        public FileSource(string path)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                throw new FileNotFoundException($"File {path} not found");
+            }
+
+            this.path = path;
+        }
+
+        public string Read() => System.IO.File.ReadAllText(path);
+    }
+}
