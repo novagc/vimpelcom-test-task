2b61d4aeb6a9ae46db99a6b286393f5652f3cd23
novagc <novagc@yandex.ru>
2020-08-03 21:06:02 +0900

    add task 2 io tools and validator

---
+++ task 2/WordCounter/WordCounter/Other/ToolsIO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WordCounter.Other
+{
+    static class ToolsIO
+    {
+        public static string[] GetTextFilesPaths(string path) => Directory.GetFiles(path, "*.txt");
+    }
+}
---
+++ task 2/WordCounter/WordCounter/Other/ValidatorIO.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace WordCounter.Other
+{
+    public static class ValidatorIO
+    {
+        public static bool VerifyDirectoryPath(string path) => Directory.Exists(path);
+
+        public static bool CheckTextFilesExistence(string path) => ToolsIO.GetTextFilesPaths(path).Length > 0;
+    }
+}
